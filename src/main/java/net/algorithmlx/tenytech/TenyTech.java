f23cac3d283b73afb55348f334531022b102b1c9
Ivan <46783751+AlgorithmLX@users.noreply.github.com>
2022-05-30 17:37:51 +0700

    fixing

--- src/main/java/net/algorithmlx/tenytech/TenyTech.java
+++ src/main/java/net/algorithmlx/tenytech/TenyTech.java
@@ -3,12 +3,8 @@ package net.algorithmlx.tenytech;
 import net.algorithmlx.tenytech.setup.Constant;
 import net.algorithmlx.tenytech.setup.ModSetup;
 import net.algorithmlx.tenytech.setup.Registry;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -19,13 +15,6 @@ public class TenyTech {
 
     public TenyTech() {
         Registry.init();
-    }
-    @SubscribeEvent
-    public static void onRegisterItems(final RegistryEvent.Register<Item> event) {
-        Registry.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(block -> {
-            event.getRegistry()
-                    .register(new BlockItem(block, new Item.Properties().tab(ModSetup.TENY_TECH))
-                        .setRegistryName(block.getRegistryName()));
-        });
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(ModSetup::init);
     }
 }
